f9fe3e2ba2bf8186ea5cb7dfcea476ae8db5e1a5
yashita99 <45969204+yashita99@users.noreply.github.com>
2021-05-29 14:50:41 +0530

    Update Ff.vue

--- components/Ff.vue
+++ components/Ff.vue
@@ -38,14 +38,6 @@ export default {
 </script>
 
 
-<!--<style>
-@import '../node_modules/@syncfusion/ej2-base/styles/material.css';  
-@import '../node_modules/@syncfusion/ej2-buttons/styles/material.css';  
-@import '../node_modules/@syncfusion/ej2-calendars/styles/material.css';  
-@import '../node_modules/@syncfusion/ej2-dropdowns/styles/material.css';  
-@import '../node_modules/@syncfusion/ej2-inputs/styles/material.css';  
-@import '../node_modules/@syncfusion/ej2-navigations/styles/material.css';
-@import '../node_modules/@syncfusion/ej2-popups/styles/material.css';
-@import '../node_modules/@syncfusion/ej2-splitbuttons/styles/material.css';
+<style>
 @import "../node_modules/@syncfusion/ej2-vue-grids/styles/material.css";
-</style>-->
+</style>
